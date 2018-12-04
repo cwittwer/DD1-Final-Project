a4e727e1ffa0b06abfc91aaa2928e35719e04eab
Brian <blev@vt.edu>
2018-12-04 16:31:15 -0500

    sorry update the file in starter verilog now

--- starter_verilog/intxnCtrl.v
+++ starter_verilog/intxnCtrl.v
@@ -13,13 +13,95 @@
 
 //         NOTE: reset is active-low
 
-module intxnCtrl(clock, reset_n, car_detected, lights_out);
 
-   input        clock;        // 50MHz (?) clock
-   input        reset_n;      // active-low reset
-   input        car_detected; // active-high enable
-   output [5:0] lights_out;   // 6-bit output (2 sets of 3 bits - one per LED)
+module intxnCtrl(clk, reset_n, car, lights);
+	input 				clk, car, reset_n;
+	output	reg	[6:0]	lights;
+	
+	reg					reset;
+	reg 		[15:0]	Top;
+	reg 		[11:0]	Bottom;
+	reg			[2:0] 	sec;
+	reg			[1:0]	state, prev_state; // 00: GNS, 01: YNS, 10 GEW, 11 YEW; 
 
-   // xyz etc
+initial begin
+	Top = 16'h0000;
+	Bottom = 12'h000;
+	state = 2'b00;
+	sec = 3'b0;
+	reset = 1'b0;
+end
+	
+always @(posedge clk)begin
+	case (state)
+		3'b00:
+			lights <= 6'b100001;
+		3'b01:
+			lights <= 6'b010001;
+		3'b10:
+			lights <= 6'b001100;
+		3'b11:
+			lights <= 6'b001010;
+	endcase
+end
+
+always @(*)begin
+	prev_state = state;
+	if(reset_n == 1'b0) begin
+		reset <= 1'b1;
+		state <= 2'b00;
+	end
+	else if(reset == 1'b1)
+		reset = 1'b0;
+	case(state)
+		2'b00: begin
+			if (car == 1'b1) begin
+				reset <= 1'b1;
+				state <= 2'b01;
+			end
+		end
+		2'b01: begin
+			if (sec >= 3'd1)begin
+				state <= 2'b10;
+				reset <= 1'b1;
+			end
+		end
+		3'b10: begin
+			if(sec >= 3'd5) begin
+				state <= 2'b11;
+				reset <= 1'b1;
+			end
+		end
+		3'b11: begin
+			if(sec >= 3'b1) begin
+				state <= 2'b00;
+				reset <= 1'b1;
+			end
+		end
+	endcase
+end 
+always @(posedge clk or posedge reset)begin
+	
+	if(reset == 1'b1) begin
+		sec <= 3'b000;
+		Top <= 16'h0000;
+		Bottom <= 12'h000;
+	end
+	else if (Top == 16'h2FAF && Bottom == 12'h080) begin
+		Top <= 16'h0000;
+		Bottom <= 12'h000;
+		
+		sec = sec + 3'b001;
+	end
+	else if(Bottom == 12'hFFF)begin
+		Bottom <= 12'h000;
+		Top <= Top + 16'h0001;
+	end
+	else
+		Bottom <= Bottom + 12'b000000000001;
+
+
+end
+
 
 endmodule
